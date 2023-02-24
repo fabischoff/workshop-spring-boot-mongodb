ce34da647f99989af4ffe5554846023f7eb2fde6
fausto bischoff <fabischoff@yahoo.com.br>
2023-02-24 11:27:41 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/infabis/workshopmongo/service/UserService.java
+++ src/main/java/br/com/infabis/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package br.com.infabis.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.infabis.workshopmongo.domain.User;
 import br.com.infabis.workshopmongo.repository.UserRepository;
+import br.com.infabis.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById (String id) {
+		Optional<User> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
