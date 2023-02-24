1057db8efb5c4e6f60345a204c6f7f4d72174e07
fausto bischoff <fabischoff@yahoo.com.br>
2023-02-24 11:50:15 -0300

    Inserção de usuário com POST

--- src/main/java/br/com/infabis/workshopmongo/service/UserService.java
+++ src/main/java/br/com/infabis/workshopmongo/service/UserService.java
@@ -7,23 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.infabis.workshopmongo.domain.User;
+import br.com.infabis.workshopmongo.dto.UserDTO;
 import br.com.infabis.workshopmongo.repository.UserRepository;
 import br.com.infabis.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	public User findById (String id) {
+
+	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
