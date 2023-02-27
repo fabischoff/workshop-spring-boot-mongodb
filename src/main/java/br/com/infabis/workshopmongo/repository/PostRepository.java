07f4108c459b565e24ce4eee1dfb39c0e8375dea
fausto bischoff <fabischoff@yahoo.com.br>
2023-02-27 11:11:03 -0300

    Consulta simples com query methods

--- src/main/java/br/com/infabis/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/com/infabis/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package br.com.infabis.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.infabis.workshopmongo.domain.Post;
 
 public interface PostRepository  extends MongoRepository<Post, String>{
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/br/com/infabis/workshopmongo/resources/PostResource.java
+++ src/main/java/br/com/infabis/workshopmongo/resources/PostResource.java
@@ -1,25 +1,37 @@
 package br.com.infabis.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.infabis.workshopmongo.domain.Post;
+import br.com.infabis.workshopmongo.resources.util.URL;
 import br.com.infabis.workshopmongo.service.PostService;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResource {
-	
+
 	@Autowired
 	private PostService service;
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
 }
